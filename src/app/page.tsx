aee1ecbf3360fed15f160dfef5299c08ea23e43c
markedzel <mark.edzel.potane@altus.digital>
2023-03-06 10:46:10 +0800

    Add favicons, images & CSS Reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { Manrope } from 'next/font/google';
+
+const manrope = Manrope({ subsets: ['latin'] });
+
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <section>
+        <div>
+          <h1 className={manrope.className}>Welcome to Altus Digital</h1>
+        </div>
+      </section>
+    </main>
+  );
 }
