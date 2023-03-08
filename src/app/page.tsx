e67ba565d4d60ea939a6ce37155bbe55b1a6c30f
markedzel <mark.edzel.potane@altus.digital>
2023-03-08 08:25:05 +0800

    Update metadata and add SVG icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { KoHo } from 'next/font/google';
 import Image from 'next/image';
+import Button from 'src/components/Button';
+import Footer from 'src/components/Footer';
 
 const koho = KoHo({
   weight: ['400', '500', '700'],
@@ -13,18 +17,13 @@ export default function Home() {
       <header>
         <nav>
           <div className="py-10 px-5 fixed">
-            <p className="border border-violet-500 rounded-2xl p-2 bg-gradient-to-r from-gray-200 via-slate-400 to-gray-300">
-              Get Started by Editing{' '}
-              <code className={`font-bold ${koho.className}`}>
-                src/app/page.tsx
-              </code>
-            </p>
+            <Button />
           </div>
         </nav>
       </header>
       <main>
         <section>
-          <div className="grid place-items-center min-h-[666px]">
+          <div className="pt-40 grid place-items-center grid-cols-1 gap-10">
             <div className="flex gap-5 items-center justify-center">
               <Image
                 src="/images/altusdigital-purple-logo.png"
@@ -40,30 +39,26 @@ export default function Home() {
                 </span>
               </h1>
             </div>
-          </div>
-          <div className='pb-10 px-20'>
-            <div className="flex items-center justify-center gap-5 flex-col md:flex-row">
-              <div className='p-10 border border-violet-300 rounded-2xl cursor-pointer'>
-                Docs {'->'}
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-              </div>
-              <div className='p-10 border border-violet-300 rounded-2xl cursor-pointer'>
-                Templates {'->'}
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-              </div>
-              <div className='p-10 border border-violet-300 rounded-2xl cursor-pointer'>
-                Deploy {'->'}
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <div className="pb-10 px-20">
+              <div className="flex items-center justify-center gap-5 flex-col md:flex-row">
+                <div className="p-10 border border-violet-300 rounded-2xl cursor-pointer hover:bg-violet-300">
+                  Docs {'->'}
+                  <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div className="p-10 border border-violet-300 rounded-2xl cursor-pointer hover:bg-violet-300">
+                  Templates {'->'}
+                  <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div className="p-10 border border-violet-300 rounded-2xl cursor-pointer hover:bg-violet-300">
+                  Deploy {'->'}
+                  <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
               </div>
             </div>
           </div>
         </section>
       </main>
-      <footer>
-        <div className="flex items-center justify-center text-slate-200 bg-slate-500 p-5 font-bold">
-          Copyright &#169; Altus Digital
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,162 @@
+import { KoHo } from 'next/font/google';
+
+import Image from 'next/image';
+import Icon1 from 'src/components/icon1.svg';
+import Icon2 from 'src/components/icon2.svg';
+import Icon3 from 'src/components/icon3.svg';
+
+const koho = KoHo({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export default function Button() {
+  return (
+    <>
+      <style jsx>{`
+        button {
+          position: relative;
+          padding: 15px 45px;
+          background: #ac95fe;
+          font-size: 17px;
+          font-weight: 500;
+          color: #181818;
+          border: 1px solid #ac95fe;
+          border-radius: 8px;
+          filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2));
+        }
+
+        button:hover {
+          border: 1px solid #9582f3;
+          background: linear-gradient(
+            85deg,
+            #a895fe,
+            #b297fc,
+            #9b94fa,
+            #a499fa,
+            #af9efa
+          );
+          animation: wind 2s ease-in-out infinite;
+        }
+
+        @keyframes wind {
+          0% {
+            background-position: 0% 50%;
+          }
+
+          0% {
+            background-position: 50% 100%;
+          }
+
+          0% {
+            background-position: 0% 50%;
+          }
+        }
+
+        .ico1 {
+          position: absolute;
+          top: 0;
+          right: 0;
+          width: 25px;
+          transform-origin: 0 0;
+          transform: rotate(10deg);
+          transition: all 0.5s ease-in-out;
+          filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.3));
+        }
+
+        button:hover .ico1 {
+          animation: slay-1 3s cubic-bezier(0.52, 0, 0.58, 1) infinite;
+          transform: rotate(10deg);
+        }
+
+        @keyframes slay-1 {
+          0% {
+            transform: rotate(10deg);
+          }
+
+          50% {
+            transform: rotate(-5deg);
+          }
+
+          100% {
+            transform: rotate(10deg);
+          }
+        }
+
+        .ico2 {
+          position: absolute;
+          top: 0;
+          left: 25px;
+          width: 12px;
+          transform-origin: 50% 0;
+          transform: rotate(10deg);
+          transition: all 1s ease-in-out;
+          filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.5));
+        }
+
+        button:hover .ico2 {
+          animation: slay-2 3s cubic-bezier(0.52, 0, 0.58, 1) 1s infinite;
+          transform: rotate(0);
+        }
+
+        @keyframes slay-2 {
+          0% {
+            transform: rotate(0deg);
+          }
+
+          50% {
+            transform: rotate(15deg);
+          }
+
+          100% {
+            transform: rotate(0);
+          }
+        }
+
+        .ico3 {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 18px;
+          transform-origin: 50% 0;
+          transform: rotate(-5deg);
+          transition: all 1s ease-in-out;
+          filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.5));
+        }
+
+        button:hover .ico3 {
+          animation: slay-3 2s cubic-bezier(0.52, 0, 0.58, 1) 1s infinite;
+          transform: rotate(0);
+        }
+
+        @keyframes slay-3 {
+          0% {
+            transform: rotate(0deg);
+          }
+
+          50% {
+            transform: rotate(-5deg);
+          }
+
+          100% {
+            transform: rotate(0);
+          }
+        }
+      `}</style>
+      <button>
+        Get Started by Editing{' '}
+        <code className={`font-bold ${koho.className}`}>src/app/page.tsx</code>
+        <div className="ico1">
+          <Image src={Icon1} alt="Icon1 leaf" />
+        </div>
+        <div className="ico2">
+          <Image src={Icon2} alt="Icon2 leaf" />
+        </div>
+        <div className="ico3">
+          <Image src={Icon3} alt="Icon3 leaf" />
+        </div>
+      </button>
+    </>
+  );
+}
