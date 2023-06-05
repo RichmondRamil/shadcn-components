cb5f7af26ae50f0beb4b72f81c551a3129114bf9
Shuaib <shuaib.abdurahman@altus.digital>
2023-06-05 18:28:44 +0800

    fix api services

--- client/src/services/APIService.ts
+++ client/src/services/APIService.ts
@@ -1,30 +1,37 @@
 // DEPENDENCIES
 import axios from 'axios';
+// CONFIG
+import config from '../shared/config';
+
 interface IRequestParams {
   url: string;
-  method: string;
   data?: object;
   params?: object;
 }
-import config from '../shared/config';
+class APIService {
+  protected baseURL: string = config.API_URL;
 
-export const requestApiService = async ({
-  url,
-  method,
-  data,
-  params,
-}: IRequestParams) => {
-  try {
-    const buildURL = `${config.API_URL}/${url}`;
-
-    const resultApiRequest = await axios({
-      method: method,
-      url: buildURL,
-      data,
-      params,
-    });
-    return resultApiRequest.data;
-  } catch (error) {
-    return Promise.reject(error);
+  public buildURL(url: string): string {
+    return this.baseURL + '/' + url;
   }
-};
+
+  public async get({ url, params }: IRequestParams) {
+    const result = await axios.get(this.buildURL(url), { params });
+
+    return result ? result.data : null;
+  }
+  public async post({ url, data, params }: IRequestParams) {
+    const result = await axios.post(url, data, { params });
+    return result ? result.data : null;
+  }
+  public async put({ url, data, params }: IRequestParams) {
+    const result = await axios.put(url, data, { params });
+    return result ? result.data : null;
+  }
+  public async remove({ url, params }: IRequestParams) {
+    const result = await axios.delete(url, { params });
+    return result ? result.data : null;
+  }
+}
+
+export default APIService;
