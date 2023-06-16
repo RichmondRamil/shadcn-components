69aded62f53b9b1cd358390344acc76161fd8868
Hiccup-Dusk <shuaib.abdurahman22@gmail.com>
2023-06-16 14:47:38 +0800

    added SDK packages

---
+++ server/src/shared/services/sdk/index.ts
@@ -0,0 +1,16 @@
+// UTILS
+import { decrypt } from '../../utils/crypto';
+import RestService from './RestService';
+import SoapServices from './SoapService';
+import IAuthObj from '../../interfaces/IAuthObj';
+
+export default class SDKServices {
+  public rest: RestService;
+  public soap: SoapServices;
+  constructor(token: string) {
+    const authObj: IAuthObj = decrypt(token);
+
+    this.rest = new RestService(authObj);
+    this.soap = new SoapServices(authObj);
+  }
+}
