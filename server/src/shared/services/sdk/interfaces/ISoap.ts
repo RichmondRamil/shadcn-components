218d9b6ef51e1033b580235bc3835b4008e80213
Hiccup-Dusk <shuaib.abdurahman22@gmail.com>
2023-07-27 23:09:01 +0800

    new changes to the soap interfaces

--- server/src/shared/services/sdk/interfaces/ISoap.ts
+++ server/src/shared/services/sdk/interfaces/ISoap.ts
@@ -1,181 +1,176 @@
 // ============== INTERFACE ==============
 export interface IApiRequestParams {
-  action: string;
-  req: Record<string, any>;
-  key: string;
+	action: string;
+	req: Record<string, any>;
+	key: string;
 }
 
 // ------------ REQUESTS ------------
 export interface IRetrieveRequest {
-  ObjectType: string;
-  Properties: string[];
-  ContinueRequest?: string;
-  Filter?: IFilter;
-  PartnerProperties?: string[];
-  QueryAllAccounts?: boolean;
-  RepeatLastResult?: any;
-  RetrieveAllSinceLastBatch?: any;
-  ClientIDs?: {
-    ClientID: string | string[];
-  };
-  Options?: IRetrieveOptions;
+	ObjectType: string;
+	Properties: string[];
+	ContinueRequest?: string;
+	Filter?: IFilter;
+	PartnerProperties?: string[];
+	QueryAllAccounts?: boolean;
+	RepeatLastResult?: any;
+	RetrieveAllSinceLastBatch?: any;
+	ClientIDs?: {
+		ClientID: string | string[];
+	};
+	Options?: IRetrieveOptions;
 }
 
 export interface ICreateRequest {
-  ObjectType: string;
-  Properties: (string | IAPIObject)[];
-  Options?: ICreateOptions;
-  RequestID?: string;
+	ObjectType: string;
+	Properties: (string | IAPIObject)[];
+	Options?: ICreateOptions;
+	RequestID?: string;
 }
 
 export interface IUpdateRequest {
-  Objects: IObjects[];
-  Options?: IUpdateOptions;
-  RequestID?: string;
+	Objects: IObjects[];
+	Options?: IUpdateOptions;
+	RequestID?: string;
 }
 
 export interface IDeleteRequest {
-  ObjectType: string;
-  Properties: (string | IAPIObject)[];
-  Options?: IDeleteOptions;
-  RequestID?: string;
+	ObjectType: string;
+	Properties: (string | IAPIObject)[];
+	Options?: IDeleteOptions;
+	RequestID?: string;
 }
 
 export interface IScheduleRequest {
-  Action: string;
-  Interactions: string[];
-  Options: IScheduleOptions;
-  OverallStatus: string;
-  OverallStatusMessage: string;
-  RequestID: string;
-  Schedule: string;
-  ObjectType: string;
+	Action: string;
+	Interactions: string[];
+	Options: IScheduleOptions;
+	OverallStatus: string;
+	OverallStatusMessage: string;
+	RequestID: string;
+	Schedule: string;
+	ObjectType: string;
 }
 
 export interface IExecuteRequest {
-  RequestID?: string;
-  Requests?: {
-    Client?: {
-      ClientID: string | string[];
-    };
-    Name: string;
-    Parameters?: { [key: string]: any };
-  };
+	RequestID?: string;
+	Requests?: {
+		Client?: {
+			ClientID: string | string[];
+		};
+		Name: string;
+		Parameters?: { [key: string]: any };
+	};
 }
 
 export interface IPerformRequest {
-  Action: string;
-  Definitions: IAPIObject[];
-  Options: IPerformOptions;
-  OverallStatus: string;
-  OverallStatusMessage: string;
-  RequestID?: string;
+	Action: string;
+	Definitions: IAPIObject[];
+	Options: IPerformOptions;
+	OverallStatus: string;
+	OverallStatusMessage: string;
+	RequestID?: string;
 }
 
 // ------------ OPTIONS ------------
 export interface IRetrieveOptions extends IDefaultOptions {
-  BatchSize?: number;
-  IncludeObjects?: boolean;
-  OnlyIncludeBase?: boolean;
-  QueuePriority?: string;
+	BatchSize?: number;
+	IncludeObjects?: boolean;
+	OnlyIncludeBase?: boolean;
+	QueuePriority?: string;
 }
 
 export interface ICreateOptions extends IDefaultOptions {
-  Container?: {
-    ContainerID: string | string[];
-  };
+	Container?: {
+		ContainerID: string | string[];
+	};
 }
 
 export interface IUpdateOptions extends IDefaultOptions {
-  Container?: {
-    ContainerID: string | string[];
-  };
+	Container?: {
+		ContainerID: string | string[];
+	};
 }
 
 export interface IDeleteOptions extends IDefaultOptions {
-  QueuePriority?: string;
+	QueuePriority?: string;
 }
 
 export interface IScheduleOptions extends IDefaultOptions {
-  QueuePriority?: string;
+	QueuePriority?: string;
 }
 
 export interface IPerformOptions extends IDefaultOptions {
-  Explanation?: string;
-  QueuePriority?: string;
+	Explanation?: string;
+	QueuePriority?: string;
 }
 
 export interface IDefaultOptions {
-  CallsInConversation?: number;
-  Client?: {
-    ClientID: string | string[];
-  };
-  ConversationID?: string;
-  Priority?: string;
-  RequestType?: string;
-  SaveOptions?: any;
-  ScheduledTime?: string;
-  SendResponseTo?: any;
-  SequenceCode?: string;
+	CallsInConversation?: number;
+	Client?: {
+		ClientID: string | string[];
+	};
+	ConversationID?: string;
+	Priority?: string;
+	RequestType?: string;
+	SaveOptions?: any;
+	ScheduledTime?: string;
+	SendResponseTo?: any;
+	SequenceCode?: string;
 }
 
 interface ISimpleFilter {
-  Property: string;
-  SimpleOperator: ESimpleOperator | string;
-  Value: string;
+	Property: string;
+	SimpleOperator: ISimpleOperator;
+	Value: string;
 }
 
 interface IComplexFilter {
-  LeftOperand: ISimpleFilter | IComplexFilter;
-  LogicalOperator: ELogicalOperator;
-  RightOperand: ISimpleFilter | IComplexFilter;
+	LeftOperand: ISimpleFilter | IComplexFilter;
+	LogicalOperator: 'AND' | 'OR';
+	RightOperand: ISimpleFilter | IComplexFilter;
 }
 
 export type IFilter = ISimpleFilter | IComplexFilter;
 // ============== ENUMS ==============
-enum ESimpleOperator {
-  equals = 'equals',
-  notEquals = 'notEquals',
-  greaterThan = 'greaterThan',
-  lessThan = 'lessThan',
-  isNotNull = 'isNotNull',
-  isNull = 'isNull',
-  greaterThanOrEqual = 'greaterThanOrEqual',
-  lessThanOrEqual = 'lessThanOrEqual',
-  between = 'between',
-  IN = 'IN',
-  like = 'like',
-}
 
-enum ELogicalOperator {
-  AND = 'AND',
-  OR = 'OR',
-}
+type ISimpleOperator =
+	| 'equals'
+	| 'notEquals'
+	| 'greaterThan'
+	| 'lessThan'
+	| 'isNotNull'
+	| 'isNull'
+	| 'greaterThanOrEqual'
+	| 'lessThanOrEqual'
+	| 'between'
+	| 'IN'
+	| 'like';
 
 // ============== EXTRAS ==============
 interface IAPIObject {
-  Client?: {
-    ClientID: string | string[];
-  };
-  CorrelationID?: string;
-  CreatedDate?: string;
-  CustomerKey?: string;
-  ID?: string;
-  ModifiedDate?: string;
-  ObjectID?: string;
-  ObjectState?: string;
-  Owner?: {
-    OwnerID: string;
-    OwnerType: string;
-  };
-  PartnerKey?: string;
-  PartnerProperties?: {
-    Name: string;
-    Value: string;
-  }[];
+	Client?: {
+		ClientID: string | string[];
+	};
+	CorrelationID?: string;
+	CreatedDate?: string;
+	CustomerKey?: string;
+	ID?: string;
+	ModifiedDate?: string;
+	ObjectID?: string;
+	ObjectState?: string;
+	Owner?: {
+		OwnerID: string;
+		OwnerType: string;
+	};
+	PartnerKey?: string;
+	PartnerProperties?: {
+		Name: string;
+		Value: string;
+	}[];
 }
 
 export interface IObjects {
-  ObjectType: string;
-  Properties: (string | IAPIObject)[];
+	ObjectType: string;
+	Properties: IAPIObject;
 }
