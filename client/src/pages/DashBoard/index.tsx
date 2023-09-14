d4e7ec02b246ca4938799c5bcfd3fc55fd883975
Hiccup-Dusk <shuaib.abdurahman22@gmail.com>
2023-09-15 02:46:44 +0800

    fix ts alias compiling, added new packages, remove unused packages & files and added new config

--- client/src/pages/DashBoard/index.tsx
+++ client/src/pages/DashBoard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 // DEPENDENCIES
 // SERVICES
 import AdminService from '../../services/AdminService';
@@ -8,23 +8,23 @@ const adminService = new AdminService();
 // export interface IDashboardProps {}
 
 export default function Dashboard(/* props: IDashboardProps */) {
-  const [testState, setTestState] = useState(false);
+	const [testState, setTestState] = useState(false);
 
-  async function getAdmin() {
-    const randomNumber = Math.floor(Math.random() * 100);
-    const data = await adminService.getAdmins(randomNumber);
-    console.log(data);
-  }
+	async function getAdmin() {
+		const randomNumber = Math.floor(Math.random() * 100);
+		const data = await adminService.getAdmins(randomNumber);
+		console.log(data);
+	}
 
-  return (
-    <div>
-      <h1 className="text-4xl font-bold text-primary">Tech Stack Framework</h1>
-      <button onClick={() => getAdmin()} className="btn">
-        Test 1
-      </button>
-      <button onClick={() => setTestState(!testState)} className="btn">
-        Test 1
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-4xl font-bold text-primary">Tech Stack Framework</h1>
+			<button onClick={() => getAdmin()} className="btn">
+				Test 1
+			</button>
+			<button onClick={() => setTestState(!testState)} className="btn">
+				Test 1
+			</button>
+		</div>
+	);
 }
