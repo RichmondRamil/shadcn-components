7cce2689a6524fd4dba2c6bdd6c65a1020d9b0b6
shuaib <shuaib.abdurahman22@gmail.com>
2023-12-20 15:58:49 +0800

    Update Vite configuration to load environment variables

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,19 @@
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@shadcn': path.resolve(__dirname, './src/dependencies/shadcn'),
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	return defineConfig({
+		plugins: [react()],
+		server: {
+			port: parseInt(process.env.VITE_PORT),
 		},
-	},
-});
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+				'@shadcn': path.resolve(__dirname, './src/dependencies/shadcn'),
+			},
+		},
+	});
+};
